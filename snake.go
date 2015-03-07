6fa2f949845d4b198542b831bcd98bd161f22abc
Adrien Jarthon <jobs@adrienjarthon.com>
2015-03-07 17:30:00 +0100

    added solving method, works great ;)

--- snake.go
+++ snake.go
@@ -14,15 +14,37 @@ type Vertex struct {
 
 type Solution struct {
 	Pos [Size]Vertex
+	Grid [4][4][4]int
 	Placed int
 }
 
+func (v2 *Vertex) minus(v1 Vertex) Vertex {
+	return Vertex{v2.X - v1.X, v2.Y - v1.Y, v2.Z - v1.Z}
+}
+
+func (v1 *Vertex) plus(v2 Vertex) Vertex {
+	return Vertex{v2.X + v1.X, v2.Y + v1.Y, v2.Z + v1.Z}
+}
+
+func (sol *Solution) place(pos Vertex) bool {
+	// fmt.Println("place:", sol.Placed+1, "@", pos)
+	if pos.X < 0 || pos.X > 3 || pos.Y < 0 || pos.Y > 3 || pos.Z < 0 || pos.Z > 3 { return false; }
+	if sol.Grid[pos.X][pos.Y][pos.Z] != 0 { return false; }
+	sol.Pos[sol.Placed] = pos
+	sol.Grid[pos.X][pos.Y][pos.Z] = sol.Placed + 1
+	sol.Placed++
+	return true
+}
+
+func (sol *Solution) removeLast() bool {
+	if sol.Placed <= 0 { return false }
+	pos := sol.Pos[sol.Placed-1]
+	sol.Grid[pos.X][pos.Y][pos.Z] = 0
+	sol.Placed--
+	return true
+}
+
 func printPuzzle(sol Solution) {
-	var grid [4][4][4]int
-	for i := 0; i < sol.Placed; i++ {
-		pos := sol.Pos[i]
-		grid[pos.X][pos.Y][pos.Z] = i+1
-	}
 	fmt.Print("        ")
 	for z := 0; z < 4; z++ {
 		fmt.Print("    z: ", z, "\t")
@@ -32,8 +54,8 @@ func printPuzzle(sol Solution) {
 		fmt.Print(" y: ", y, "   ")
 		for z := 0; z < 4; z++ {
 			for x := 0; x < 4; x++ {
-				if grid[x][y][z] > 0 {
-					fmt.Printf("%2d ", grid[x][y][z])
+				if sol.Grid[x][y][z] > 0 {
+					fmt.Printf("%2d ", sol.Grid[x][y][z])
 				} else {
 					fmt.Print(" - ")
 				}
@@ -44,12 +66,56 @@ func printPuzzle(sol Solution) {
 	}
 }
 
+func solvePuzzle(sol *Solution) (bool) {
+	if sol.Placed == Size {
+		return true
+	} else {
+		last_pos := sol.Pos[sol.Placed-1]
+		direction := last_pos.minus(sol.Pos[sol.Placed-2])
+		// fmt.Println("last_pos:", last_pos, "direction:", direction, "angled:", Angled[sol.Placed-1])
+		if Angled[sol.Placed-1] == 1 {
+			choices := []Vertex{}
+			if direction.X != 0 {
+				choices = []Vertex{Vertex{0, 1, 0}, Vertex{0, -1, 0}, Vertex{0, 0, 1}, Vertex{0, 0, -1}}
+			} else if direction.Y != 0 {
+				choices = []Vertex{Vertex{1, 0, 0}, Vertex{-1, 0, 0}, Vertex{0, 0, 1}, Vertex{0, 0, -1}}
+			} else {
+				choices = []Vertex{Vertex{0, 1, 0}, Vertex{0, -1, 0}, Vertex{1, 0, 0}, Vertex{-1, 0, 0}}
+			}
+			for _, choice := range choices {
+				if sol.place(last_pos.plus(choice)) {
+					if solvePuzzle(sol) {
+						return true
+					} else {
+						sol.removeLast();
+					}
+				}
+			}
+			return false
+		} else {
+			if sol.place(last_pos.plus(direction)) {
+				if solvePuzzle(sol) {
+					return true
+				} else {
+					sol.removeLast();
+					return false
+				}
+			} else {
+				return false
+			}
+		}
+	}
+}
+
 func main() {
 	var sol Solution
-	sol.Pos[0] = Vertex{0, 0, 0}
-	sol.Pos[1] = Vertex{1, 0, 0}
-	sol.Pos[2] = Vertex{2, 0, 0}
-	sol.Pos[3] = Vertex{2, 1, 0}
-	sol.Placed = 4
+	sol.place(Vertex{0, 0, 0})
+	sol.place(Vertex{1, 0, 0})
+	sol.place(Vertex{2, 0, 0})
+	sol.place(Vertex{2, 1, 0})
+	fmt.Println("\n Start:")
+	printPuzzle(sol)
+	res := solvePuzzle(&sol);
+	fmt.Println("\n Solved:", res)
 	printPuzzle(sol)
 }
